923f46b734837cc82e6bd39dd8af0ae82662514f
Verrer <joshkeif@gmail.com>
2020-02-01 11:34:13 -0500

    Added Readme.md & formatted output for grading requirements

---
+++ readme.md
@@ -0,0 +1,7 @@
+Joshua Keif
+
+Runs automatically and the environment is setup with gradle
+
+WebJsonGrabber pulls from githubs job positions, loops until empty pages are found, and converts the page using gson into a JobPost class structure
+
+Junit(2 appropriate test) & Google Java linter.
--- src/main/java/WebJsonGrabber.java
+++ src/main/java/WebJsonGrabber.java
@@ -7,23 +7,23 @@ import java.util.*;
 public class WebJsonGrabber {
     public static void main(String[] args) {
         //Structs items
-        List<JobPost> jobLists = new ArrayList<JobPost>();
-        List<JobPost> temp = new ArrayList<JobPost>();
+        List<JobPost> jobLists = new ArrayList<>();
+        List<JobPost> temp = new ArrayList<>();
         URLDownloader downloader = new URLDownloader();
         String url = "https://jobs.github.com/positions.json?page=";
         int pageNumber = 1;
-        //URL data defined
+        //Adds data for the loop to run
         String jsonData = downloader.URLSelector(url);
         //System.out.println(downloader.URLSelector(url));
 
         //Gson Configuration
         GsonBuilder builder = new GsonBuilder();
-        //builder.setPrettyPrinting();
+        builder.setPrettyPrinting();
         Gson gson = builder.create();
 
         while(jsonData != null)
         {
-            jsonData = downloader.URLSelector(url + Integer.toString(pageNumber));
+            jsonData = downloader.URLSelector(url + pageNumber);
             temp = gson.fromJson(jsonData, new TypeToken<List<JobPost>>() {}.getType());
             Optional.ofNullable(temp).ifPresent(jobLists::addAll);
             pageNumber++;
@@ -33,11 +33,12 @@ public class WebJsonGrabber {
         {
             System.out.println("No more webpages to pull from");
         }
+
         int i = 0;
         for (JobPost jobList : jobLists) {
-            System.out.println(i);
+            System.out.println("JobList Entry" + i);
             i++;
-            //System.out.println(jobList.toString());
+            System.out.println(jobList.toString());
         }
     }
 }
