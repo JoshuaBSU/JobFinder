a63a357ade5bb090831dd3b34aaf99345d8cf6a6
Verrer <joshkeif@gmail.com>
2020-02-03 18:47:19 -0500

    Test 2

--- src/test/java/WebJsonGrabberTest.java
+++ src/test/java/WebJsonGrabberTest.java
@@ -1,17 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class WebJsonGrabberTest {
 
   @Test
-  void fileWriter()
-  {
-    //initialize
-    WebJsonGrabber run = new WebJsonGrabber();
+  void main() throws IOException {
+    String[] a = new String[0];
+    WebJsonGrabber.main(a);
     String jsontext = "";
     //job title from https://jobs.github.com/positions/4cd29974-e48b-11e8-8478-fb9810f86b79
     String knowJobTitle = "Software engineer (back end)";
